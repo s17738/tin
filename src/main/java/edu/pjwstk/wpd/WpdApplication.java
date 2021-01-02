2e8860c74644a682f1d9259be05cf7bb490f283b
s17738 <61625374+s17738@users.noreply.github.com>
2021-01-02 21:20:39 +0100

    add spring backend structure

---
+++ src/main/java/edu/pjwstk/wpd/WpdApplication.java
@@ -0,0 +1,13 @@
+package edu.pjwstk.wpd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WpdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/pjwstk/wpd/WpdApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pjwstk.wpd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WpdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
