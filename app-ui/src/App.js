cd80bcf8e1ca3dd38b5fcea28e021556355900d5
s17738 <61625374+s17738@users.noreply.github.com>
2021-01-02 21:17:09 +0100

    restructure

--- app-ui/src/App.js
+++ app-ui/src/App.js
@@ -65,28 +65,36 @@ function App() {
                                 <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
                                 <td>11-12-2020</td>
                                 <td>11-01-20201</td>
-                                <td><button className="button">Edytuj</button></td>
+                                <td>
+                                    <button className="button">Edytuj</button>
+                                </td>
                             </tr>
                             <tr>
                                 <td>Zadanie 2</td>
                                 <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
                                 <td>11-01-20201</td>
                                 <td>11-01-20201</td>
-                                <td><button className="button">Edytuj</button></td>
+                                <td>
+                                    <button className="button">Edytuj</button>
+                                </td>
                             </tr>
                             <tr>
                                 <td>Zadanie 3</td>
                                 <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
                                 <td>11-01-20201</td>
                                 <td>11-01-20201</td>
-                                <td><button className="button">Edytuj</button></td>
+                                <td>
+                                    <button className="button">Edytuj</button>
+                                </td>
                             </tr>
                             <tr>
                                 <td>Zadanie 4</td>
                                 <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
                                 <td>11-7-2020</td>
                                 <td>11-01-20201</td>
-                                <td><button className="button">Edytuj</button></td>
+                                <td>
+                                    <button className="button">Edytuj</button>
+                                </td>
                             </tr>
                             </tbody>
                         </table>
